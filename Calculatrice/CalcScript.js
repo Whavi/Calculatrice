8fe06d3f455eb4530e60179ccb6dd9b9de4d3de1
Fethi <54514676+Whavi@users.noreply.github.com>
2023-01-24 17:17:48 +0100

    Add files via upload

--- Calculatrice/CalcScript.js
+++ Calculatrice/CalcScript.js
@@ -1,17 +1,35 @@
 function fordisplay(value) {
-    document.getElementById("output").value = value;
+    document.getElementById("output").value += value;
 }
+
 
 function effacer() {
     document.getElementById("output").value = ""
 } 
+
+
 function del() {
     document.getElementById("historique").value = ""
+    document.getElementById("output").value = ""
 } 
-function solve() 
-{ 
+
+
+function solve() { 
     let a = document.getElementById("output").value 
     let b = eval(a) 
-    document.getElementById("historique").value = b 
+    document.getElementById("historique").value += " " + a +  " = " 
+    document.getElementById("historique").value += b+"\n"
+    document.getElementById("output").value = ""
+    document.getElementById("output").value += b
 } 
+
+function arret(){
+    document.getElementById("historique").value = "La calculatrice est arreter !"
+    document.getElementById("output").value = " "
+
+    document.getElementsById('l2-1').prop(disabled, true)
+
+}
+
+
 
