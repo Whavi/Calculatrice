3aaa4f9faf7b8f9584f59a287c11871333f559f5
B.Fethi <54514676+Whavi@users.noreply.github.com>
2023-02-19 17:13:56 +0100

    Add files via upload

--- Calculatrice/Calculatrice/CalcScript.js
+++ Calculatrice/Calculatrice/CalcScript.js
@@ -1,51 +1,45 @@
-var boolean = true; 
+var boolean = true;
 
 function fordisplay(value) {
-    if(boolean == false)
-    {
+    if (boolean == false) {
         console.log("")
-    }else {
-    document.getElementById("output").value += value;
+    } else {
+        document.getElementById("output").value += value;
     }
 }
 
 function effacer() {
-    if(boolean == false)
-    {
+    if (boolean == false) {
         console.log("")
     }
-    else{
+    else {
         document.getElementById("output").value = ""
     }
-} 
+}
 
 function del() {
-    if(boolean == false)
-    {
+    if (boolean == false) {
         console.log("")
-    }else {
-    document.getElementById("historique").value = ""
-    document.getElementById("output").value = ""
+    } else {
+        document.getElementById("historique").value = ""
+        document.getElementById("output").value = ""
     }
-} 
+}
 
-
-function solve() { 
-    if(boolean == false)
-    {
+function solve() {
+    if (boolean == false) {
         console.log("")
-    }else {
-    let a = document.getElementById("output").value 
-    let b = eval(a) 
-    document.getElementById("historique").value += " " + a +  " = " 
-    document.getElementById("historique").value += b+"\n"
-    document.getElementById("output").value = ""
-    document.getElementById("output").value += b
+    } else {
+        let a = document.getElementById("output").value
+        let b = eval(a)
+        document.getElementById("historique").value += " " + a + " = "
+        document.getElementById("historique").value += b + "\n"
+        document.getElementById("output").value = ""
+        document.getElementById("output").value += b
     }
-} 
+}
 
-
-function arret(){
+function arret() {
     document.getElementById("historique").value = "La calculatrice est arreter !"
     document.getElementById("output").value = " "
     boolean = false
